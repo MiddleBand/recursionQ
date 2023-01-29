b59443882de4723493c85a51dc9c4471c72d533a
Vladislav <milliganiam6@gmail.com>
2023-01-29 17:17:28 +0300

    task54 ОК

--- task54/Program.cs
+++ task54/Program.cs
@@ -1,14 +1,69 @@
-﻿Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-В итоге получается вот такой массив:
-7 4 2 1
-9 5 3 2
-8 4 4 2
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
 
+Console.Clear();
 
+void PrintArray(int[,] matr) // Создание матрицы
+{
+    for (int i = 0; i  < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int [,] matr) // Заполнение массива рандомными данными
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr [i,j] = new Random().Next(1,100);
+        }
+    }
+}
+
+void CharacterOrder(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) // Array Sorting
+    {
+        for (int j = array.GetLength(1) - 1; j > 0; j--)
+        {
+
+            for (int k = 0; k < j; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+int [, ] array = new int [5, 5];
+Console.WriteLine("Строки массива в хаотичном порядке");
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Строки массива в обратном порядке");
+CharacterOrder(array);
+PrintArray(array);
+Console.WriteLine();
 
 
 
